78043169469d31b3f79bd3c0e31381ff2b5dfd17
Rob Yeomans <robyeomans1@gmail.com>
2022-09-18 21:29:44 +0100

    resize sign up btn

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -13,6 +13,6 @@
 @section('signup')
     <div class="absolute  bottom-12 container w-screen">
         <input type="email" placeholder="Enter your email address to sign up to our latest news releases" class="p-2 w-3/5 ml-56">
-        <input type="button" value="Sign Up" class="rounded bg-pink-500 p-2 text-white hover:bg-pink-400 cursor-pointer ">
+        <input type="button" value="Sign Up" class="rounded bg-pink-500 py-2 px-10 text-white hover:bg-pink-400 cursor-pointer ml-2">
     </div>
 @endsection
